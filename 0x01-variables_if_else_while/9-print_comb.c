d728b8cad575b3a2bbcef4ef0daa74824daa4798
ali-chaf <abdelalichaafane@gmail.com>
2023-08-11 09:00:13 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int  num = '0';
+
+	for (num = '0'; num <= '9'; num++) 
+	{
+		putchar(num);
+		if (num != '9')
+			putchar(',');
+		if (num != '9')
+			putchar(' ');
+	}
+
+
+	putchar('\n');
+	return (0);
+}
