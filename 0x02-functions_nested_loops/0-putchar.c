9ec68c46467a2581c2e118d9d965f5f8a5c1ef28
ali-chaf <abdelalichaafane@gmail.com>
2023-08-15 19:16:41 +0100

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: returns 0 when done
+ */
+int main(void)
+
+{
+	putchar('_');
+	putchar('p');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
