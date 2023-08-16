7a102bd15e0f8af6b73a9ba077fcb4ddfda105c5
ali-chaf <abdelalichaafane@gmail.com>
2023-08-16 16:47:29 +0100

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,26 +4,25 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	long long first = 0;
-	long long second = 1;
-	long long next;
-	int i;
+int main() {
+    long int first = 0;
+    long int second = 1;
+    long int next;
+    int i;
+    
+    for (i = 2; i < 52; ++i)
+    {
+        next = first + second;
 
-	for (i = 2; i < 52; ++i)
-	{
-		next = first + second;
+        printf("%lld", next);
+            if(next != 20365011074)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        first = second;
+        second = next;
+    }
 
-		printf("%lld", next);
-		if (next != 20365011074)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		first = second;
-		second = next;
-	}
-
-	return (0);
+    return 0;
 }
