7d2b4ac1e85dfd82ba5b7a50c5821e7281a8055a
ali-chaf <abdelalichaafane@gmail.com>
2023-08-16 16:42:48 +0100

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
-
 /**
- * main - check the code.
+ * main - prints the first 50 Fibonacci numbers
  *
  * Return: Always 0.
  */
 int main(void)
 {
+	long long first = 0;
+	long long second = 1;
+	long long next;
 	int i;
 
-	for (i = 0; i < 9; i++)
-		printf("%d", i);
+	for (i = 2; i < 52; ++i)
+	{
+		next = first + second;
+
+		printf("%lld", next);
+		if (next != 20365011074)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		first = second;
+		second = next;
+	}
+
 	return (0);
 }
