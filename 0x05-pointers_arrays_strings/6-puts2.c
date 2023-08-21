8e84560763e836f0e8f54419e7b000255a65ffce
ali-chaf <abdelalichaafane@gmail.com>
2023-08-21 22:44:21 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,18 @@
 #include <string.h>
 #include "main.h"
-
+/**
+ * puts2 - Prints every second character of a string,
+ * followed by a new line
+ * @str: Pointer to the string to print the characters from
+ */
 void puts2(char *str)
 {
-	int i;
-	for (i = 0; i <= 9; i++)
-		if (i % 2 == 0)
+	int ali;
+
+	for (ali = 0; str[ali] != '\0'; ali++)
+		if (ali % 2 == 0)
 		{
-			_putchar(str[i]);
+			_putchar(str[ali]);
 		}
 			_putchar('\n');
 }
