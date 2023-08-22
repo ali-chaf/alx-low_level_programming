77e7f9ba9dde6eedda42b668878bdec4d108b6bc
ali-chaf <abdelalichaafane@gmail.com>
2023-08-22 01:51:03 +0100

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the integer converted from the string
+ */
 int _atoi(char *s)
 {
-   return (atoi(s));
+	return (atoi(s));
 }
