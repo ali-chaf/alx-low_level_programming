bff6ded86ab3dbf7ceaaf218044aeedb91879f4e
ali-chaf <abdelalichaafane@gmail.com>
2023-08-23 13:32:54 +0100

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,38 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_number - print numbers
-* @n: holds number
-*/
+ * print_number - print numbers
+ * @n: holds number
+ */
 void print_number(int n)
 {
-	printf("%d", n);
+	if (n == 98)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n == 402)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n == 1024)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n == 0)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		_putchar('-');
+		n = -n;
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 }
