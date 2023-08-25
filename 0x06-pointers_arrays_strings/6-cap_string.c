2ba7b2890d108dfe8e108462cae1a7755766f4ec
ali-chaf <abdelalichaafane@gmail.com>
2023-08-26 00:42:17 +0100

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,25 +6,32 @@
 *@ptr: holds the string
 *Return: return the captillzed string
 */
+int is_separator(char c)
+{
+    char separators[] = " \t\n,;.!?\"(){}";
+    for (int i = 0; separators[i] != '\0'; ++i) {
+        if (c == separators[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 char *cap_string(char *ptr)
 {
-	size_t i;
+        size_t i;
 
-	for (i = 0; i < strlen(ptr); i++)
-	{
-		if ((isspace(ptr[i - 1]) || i == 0 || (ispunct(ptr[i - 1])
-			&& ptr[i - 1] != '-')))
-		{
-			if (islower(ptr[i]))
-			{
-				ptr[i] = toupper(ptr[i]);
-			}
-		}
-		else if (isupper(ptr[i]))
-		{
-			ptr[i] = tolower(ptr[i]);
-		}
-	}
+        for (i = 0; i < strlen(ptr); i++)
+        {
+                if (is_separator(ptr[i - 1]) || i == 0)
+                {
+                        if (islower(ptr[i]))
+                        {
+                               ptr[i] = toupper(ptr[i]);
+                        }
+                }
 
-	return (ptr);
+        }
+
+        return (ptr);
 }
