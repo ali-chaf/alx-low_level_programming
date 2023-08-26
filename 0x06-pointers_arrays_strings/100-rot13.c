da794d5764d80dc505bf6c1d385b913f7b1bd1e6
ali-chaf <abdelalichaafane@gmail.com>
2023-08-26 04:04:43 +0100

    'rot13'

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,18 +1,23 @@
-#include "main.h"
 char *rot13(char *s)
 {
-    char *ptr = s;
-    while (*s != '\0')
-    {
-        if ((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M'))
-        {
-            *s += 13;
-        } 
-        else if ((*s >= 'n' && *s <= 'z') || (*s >= 'N' && *s <= 'Z'))
-        {
-            *s -= 13;
-        }
-        s++;
-    }
-    return ptr;
+	char *ptr = s;
+
+	while (*s != '\0')
+	{
+		if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+		{
+			if ((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M'))
+			{
+				*s += 13;
+			}
+			else 
+			{
+				*s -= 13;
+			}
+
+		} 
+
+		s++;
+	}
+	return ptr;
 }
