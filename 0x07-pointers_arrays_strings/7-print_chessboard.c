72eebd8972d80e01eda797e58a8c34dc6d2f304e
ali-chaf <abdelalichaafane@gmail.com>
2023-08-28 08:08:01 +0100

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces to print
+ */
+void print_chessboard(char (*a)[8])
+{
+	char i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+			printf("\n");
+	}
+}
