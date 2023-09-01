552a6275bc0f7a6a8ab4f224d0aecd376fe9c3b5
ali-chaf <abdelalichaafane@gmail.com>
2023-09-01 10:04:10 +0100

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,13 @@
 int main(int argc, char *argv[])
 {
 	int a, b, result;
-	
+
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
-	
+
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 	result = a * b;
